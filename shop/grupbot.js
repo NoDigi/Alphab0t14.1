4ba91cec91ec107ede92443556b3dd89218d651b
NoDigi <98347766+NoDigi@users.noreply.github.com>
2022-01-26 00:10:41 +0800

    Update grupbot.js

--- shop/grupbot.js
+++ shop/grupbot.js
@@ -2,14 +2,14 @@
 const gcbotwa = () =>{
 	return`Join Aja Semua Fitur Bot Bisa Digunakan !
 
-1. *Alphabot Support*
-_https://chat.whatsapp.com/EU890BcXjyBDkNaUT5WmYV_
-2. *Alphabot Support 2*
-_https://chat.whatsapp.com/E8NExJwIbhBJYzssfqJNsE_
-3. *Alphabot Support 3*
-_https://chat.whatsapp.com/KCSqHTky1apG7ApePsfiPy_
-4. *Alphabot Support 4* 
-_https://chat.whatsapp.com/KwmvHr7VMFj7r5ry9xmMsU_
+1. *Group Bot¹✨*
+_https://chat.whatsapp.com/BCkOrQkTa7pIORSOWyBJpk_
+2. *Group Bot²🥀*
+_https://chat.whatsapp.com/GzDQlgoUamS5Ok4EOcYRRB_
+3. *Group Bot¹✨*
+_https://chat.whatsapp.com/BCkOrQkTa7pIORSOWyBJpk_
+4. *Group Bot²🥀* 
+_https://chat.whatsapp.com/GzDQlgoUamS5Ok4EOcYRRB_
 
 Jika ada link yang ke reset, silahkan hubungi
 owner untuk meminta link yang baru
